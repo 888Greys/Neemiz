d4278576ebf1e4a342e8f23cb253574f535f8440
888Greys <toxicgreys001@gmail.com>
2026-06-13 08:11:32 +0300

    Redesign P2P order workspace

--- app/p2p/order/[id]/page.tsx
+++ app/p2p/order/[id]/page.tsx
@@ -4,7 +4,7 @@ import { P2POrderClient } from "@/components/p2p-order-client";
 export default async function OrderPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return (
-    <AppShell>
+    <AppShell hideFooter>
       <P2POrderClient orderId={id} />
     </AppShell>
   );
